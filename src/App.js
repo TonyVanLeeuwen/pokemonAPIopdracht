7537c9972f91347f377eb18a9cb93d3bab1dc48a
TonyVanLeeuwen <tony.vanleeuwen@novi-education.com>
2021-03-29 20:30:45 +0200

    eerste data uit de api gehaald en op DOM geplaatst

--- src/App.js
+++ src/App.js
@@ -3,7 +3,29 @@ import './App.css';
 import axios from "axios";
 
 function App() {
+    const [pokemon, setPokemon] = useState([]);
 
+    useEffect(() => {
+        async function gottaGetEmAll() {
+            try{
+                const {data} = await axios.get("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20")
+                // console.log(data.results)
+                setPokemon(data.results)
+            } catch (e) {
+                console.log(e)
+                return e;
+            }
+        }
+        gottaGetEmAll()
+    }, []);
+
+    return (
+        <ul>
+            {pokemon && pokemon.map((pokemon) => {
+                return <li key={pokemon.name}>{pokemon.name}</li>
+            })}
+        </ul>
+    );
 }
 
 export default App;
